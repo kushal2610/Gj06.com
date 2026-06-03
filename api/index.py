ac25a6c59fd0a3a9bd009f5a425429c4b8053acb
kushal2610 <kushal2624@gmail.com>
2026-06-03 16:53:26 +1000

    api routing

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-# Make sure parent directory is in path so we can import our modules
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from main import app
